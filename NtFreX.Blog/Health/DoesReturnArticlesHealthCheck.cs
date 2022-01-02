9ef34e1be29da21020168f805b60b37b40239286
NtFreX <frederik@traber.org>
2022-01-02 17:40:32 +0100

    reuse http client for multiple requests (depending on the health check live time)

--- NtFreX.Blog/Health/DoesReturnArticlesHealthCheck.cs
+++ NtFreX.Blog/Health/DoesReturnArticlesHealthCheck.cs
@@ -16,6 +16,7 @@ namespace NtFreX.Blog.Health
     public class DoesReturnArticlesHealthCheck : ApplicationHealthCheck
     {
         private readonly IHttpContextAccessor httpContextAccessor;
+        private readonly HttpClient httpClient = Configuration.Environment.IsDevelopment() ? new HttpClient(GetIgnoreCertificateValidationHandler()) : new HttpClient();
 
         public DoesReturnArticlesHealthCheck(ApplicationContextActivityDecorator traceActivityDecorator, IHttpContextAccessor httpContextAccessor)
             : base(traceActivityDecorator)
@@ -26,11 +27,9 @@ namespace NtFreX.Blog.Health
         public override async Task<HealthCheckResult> DoCheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
         {
             var url = httpContextAccessor.HttpContext.Request.Scheme + "://" + httpContextAccessor.HttpContext.Request.Host;
-
-            var httpClient = Configuration.Environment.IsDevelopment() ? new HttpClient(GetIgnoreCertificateValidationHandler()) : new HttpClient();
             var content = await httpClient.GetStringAsync($"{url}/api/article", cancellationToken);
-
             var articles = JsonConvert.DeserializeObject<IReadOnlyList<ArticleDto>>(content);
+
             if (articles.Count == 0)
                 return HealthCheckResult.Degraded("No article was returned from the web api");
 
@@ -43,7 +42,7 @@ namespace NtFreX.Blog.Health
             return HealthCheckResult.Healthy("The web api returns articles");
         }
 
-        private HttpClientHandler GetIgnoreCertificateValidationHandler()
+        private static HttpClientHandler GetIgnoreCertificateValidationHandler()
         {
             var handler = new HttpClientHandler();
             handler.ClientCertificateOptions = ClientCertificateOption.Manual;
